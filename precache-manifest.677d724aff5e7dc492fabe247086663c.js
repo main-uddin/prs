1fdf8404f59f88ef3eb31a99ef8c52b1b39dd388
Main Uddin <mdmain74@gmail.com>
2018-11-12 20:23:49 +0600

    Updates

---
+++ precache-manifest.677d724aff5e7dc492fabe247086663c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f17f1df7c4be94de2a26",
+    "url": "/prs/static/css/main.4a8c6774.chunk.css"
+  },
+  {
+    "revision": "f17f1df7c4be94de2a26",
+    "url": "/prs/static/js/main.f17f1df7.chunk.js"
+  },
+  {
+    "revision": "61a78565f5483ccc2274",
+    "url": "/prs/static/js/1.61a78565.chunk.js"
+  },
+  {
+    "revision": "491a4fde3c1518060633",
+    "url": "/prs/static/js/runtime~main.491a4fde.js"
+  },
+  {
+    "revision": "dbe812145b76d177fc194ec002b75780",
+    "url": "/prs/static/media/screaming-eagle.dbe81214.png"
+  },
+  {
+    "revision": "e292985fbebfad1cdb4fa1e18f88eb17",
+    "url": "/prs/static/media/Chateau-Margaux.e292985f.png"
+  },
+  {
+    "revision": "6d1662a766fb5a87ccdc456e36a3b548",
+    "url": "/prs/static/media/absolut-vodka.6d1662a7.png"
+  },
+  {
+    "revision": "631dc4d94aa651b51bd244a669a10dfa",
+    "url": "/prs/static/media/grey-goose.631dc4d9.png"
+  },
+  {
+    "revision": "f9567fccc0a431b514a3261c11d103fb",
+    "url": "/prs/static/media/russian-standard.f9567fcc.png"
+  },
+  {
+    "revision": "9aa6234d90b763676e349ea420cc570f",
+    "url": "/prs/static/media/pinnacle-vodka.9aa6234d.png"
+  },
+  {
+    "revision": "6d84ed90043f5a75ace7fea1f17d5593",
+    "url": "/prs/static/media/background.6d84ed90.svg"
+  },
+  {
+    "revision": "b098e17483a940bd6b8545be397fbd9b",
+    "url": "/prs/index.html"
+  }
+];
